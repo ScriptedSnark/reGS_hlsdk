2cda5f86da60acebf5f9fb1ff155538594e66fb3
ScriptedSnark <51358194+ScriptedSnark@users.noreply.github.com>
2023-01-06 20:32:41 +0500

    Dummy snd_win.cpp

---
+++ engine/snd_win.cpp
@@ -0,0 +1,56 @@
+#include "glquake.h"
+
+void Snd_ReleaseBuffer(void)
+{
+
+}
+
+void Snd_AcquireBuffer(void)
+{
+
+}
+
+void S_BlockSound(void)
+{
+
+}
+
+void S_UnblockSound(void)
+{
+
+}
+
+void FreeSound(void)
+{
+
+}
+
+sndinitstat SNDDMA_InitDirect(void)
+{
+
+}
+
+qboolean SNDDMA_InitWav(void)
+{
+
+}
+
+qboolean SNDDMA_Init(void)
+{
+
+}
+
+int SNDDMA_GetDMAPos(void)
+{
+
+}
+
+void SNDDMA_Submit(void)
+{
+
+}
+
+void SNDDMA_Shutdown(void)
+{
+
+}
